44a560158a55ea3a2f3f27c1e1b94ca05b57b81c
ARJUN <pachangedinanath@gmail.com>
2026-01-22 22:00:41 +0530

    Completed 💯

---
+++ UPPERMOON/plugins/sudo/autoend.py
@@ -0,0 +1,53 @@
+# All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from UPPERMOON import app
+from UPPERMOON.misc import SUDOERS
+from UPPERMOON.utils.database import (
+    autoend_off,
+    autoend_on,
+    autoleave_off,
+    autoleave_on,
+    is_autoend,
+    is_autoleave,
+)
+
+
+@app.on_message(filters.command("autoend") & SUDOERS)
+async def auto_end_stream(_, message: Message):
+    zerostate = await is_autoend()
+    usage = f"<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]\n\n Current state : {zerostate}"
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state in ["enable", "on", "yes"]:
+        await autoend_on()
+        await message.reply_text(
+            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+        )
+    elif state in ["disable", "off", "no"]:
+        await autoend_off()
+        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+    else:
+        await message.reply_text(usage)
+
+
+@app.on_message(filters.command("autoleave") & SUDOERS)
+async def auto_leave_chat(_, message: Message):
+    zerostate = await is_autoleave()
+    usage = f"<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoleave [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]\n\n Current state : {zerostate}"
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state in ["enable", "on", "yes"]:
+        await autoleave_on()
+        await message.reply_text(
+            "<blockquote>🛠️ ᴀᴜᴛᴏ leave chat ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ.</blockquote>"
+        )
+    elif state in ["disable", "off", "no"]:
+        await autoleave_off()
+        await message.reply_text("» ᴀᴜᴛᴏ leave chat ᴅɪsᴀʙʟᴇᴅ.")
+    else:
+        await message.reply_text(usage)
